08b7f692f7d83d3d6b3a45920a32f28a9a66fbe5
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-08-14 10:57:07 +0200

    User interface: call `MakeReservation` via HTTP `POST /reservations`

---
+++ src/app/api/reservations/route.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { PrismaReservationRepository } from "@/infrastructure/ReservationRepository/PrismaReservationRepository";
+import { MakeReservation } from "@/usecases/MakeReservation";
+
+const makeReservation = MakeReservation(new PrismaReservationRepository());
+
+const MakeReservationArguments = z.object({
+  startDate: z.string().datetime(),
+  endDate: z.string().datetime(),
+});
+
+export const POST = async (request: Request) => {
+  const body = MakeReservationArguments.safeParse(await request.json());
+
+  if (!body.success) {
+    return Response.json(
+      {
+        errorMessage: body.error,
+      },
+      { status: 400 }
+    );
+  }
+
+  try {
+    return Response.json({
+      reservation: await makeReservation(
+        new Date(body.data.startDate),
+        new Date(body.data.endDate)
+      ),
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        errorMessage: (error as Error).message,
+      },
+      { status: 400 }
+    );
+  }
+};
